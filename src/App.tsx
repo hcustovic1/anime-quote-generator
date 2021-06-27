740adde7506d330765973580cc756eea26a19142
hcustovic1 <haris.custovic@coachhub.io>
2021-06-27 20:02:22 +0200

    tailwind configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'App.css';
-import { BrowserRouter, Routes, Route, use } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home, Profile } from 'pages';
 
 function App() {
--- src/components/AnimeQuoteBlock.tsx
+++ src/components/AnimeQuoteBlock.tsx
@@ -1,13 +1,21 @@
 import { useAnime } from '../api/anime/hooks';
 
 export default function AnimeQuoteBlock() {
-  const { data, status } = useAnime();
+  const { isLoading, error, data } = useAnime();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error!</p>;
+  }
 
   console.info(data);
 
   return (
     <>
-      <span>{status}</span>
+      <span>{isLoading}</span>
     </>
   );
 }
